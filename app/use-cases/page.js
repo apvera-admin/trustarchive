d060d55d5cf34ed1e010e886561f857beffb79d1
Apvera <titus@apvera.ai>
2026-04-08 16:31:56 -0400

    Update page.js

--- app/use-cases/page.js
+++ app/use-cases/page.js
@@ -148,7 +148,7 @@ export default function UseCasesPage() {
                 }}
               >
                 {tag}
-              </link>
+              </Link>
             ))}
           </div>
         </div>
